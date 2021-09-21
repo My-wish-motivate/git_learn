489d4d7384d2635587171dbd633297a3d16f687a
kukudewo <1348643326@qq.com>
2021-09-21 20:27:38 +0800

    second commit

--- src/main/java/com/zl/git/GitTest.java
+++ src/main/java/com/zl/git/GitTest.java
@@ -8,5 +8,6 @@ package com.zl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello 1");
+        System.out.println("git hello 2");
     }
 }
