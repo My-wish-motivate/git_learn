d29f976c094df133c76dda3117d2f42731044c97
kukudewo <1348643326@qq.com>
2021-09-21 20:25:56 +0800

    first commit

---
+++ src/main/java/com/zl/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zl.git;
+
+/**
+ * @Auther: 酷酷的我
+ * @Date: 2021/9/21 20:19
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git hello 1");
+    }
+}
